ddd313142dfeb66ba93760b39ea19424099454f5
Jose Guzman <jg175415@gmail.com>
2023-04-25 11:40:05 -0500

    initial project structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Box from "@mui/material/Box";
+import MainCard from "./components/main-card/MainCard";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ "background-color": "pink" }}
+    >
+      <MainCard />
+    </Box>
   );
-}
+};
 
 export default App;
---
+++ src/components/MainCard.js
@@ -0,0 +1,13 @@
+import Button from "@mui/material/Button";
+
+const MainCard = () => {
+  const [user, setUser] = useState({});
+
+  return (
+    <Button variant="contained" color="primary">
+      Centrar botón
+    </Button>
+  );
+};
+
+export default MainCard;
