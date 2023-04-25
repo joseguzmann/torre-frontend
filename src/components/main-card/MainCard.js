407b778c5756a48405eb2e8d38a10e63af2189bf
Jose Guzman <jg175415@gmail.com>
2023-04-25 15:32:44 -0500

    feat: strength tabs added

--- src/components/main-card/MainCard.js
+++ src/components/main-card/MainCard.js
@@ -1,5 +1,18 @@
-import { Button, Container, Box } from "@mui/material";
-import { useEffect, useState } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Typography,
+  Chip,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+import { useEffect } from "react";
 import user from "./user.json";
 
 const MainCard = () => {
@@ -24,11 +37,25 @@ const MainCard = () => {
         sx={{
           display: "flex",
           alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "lightblue",
+          justifyContent: "space-around",
+          backgroundColor: "blue",
         }}
       >
-        <h1>users</h1>
+        <Typography variant="body1">{user.person.name}</Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+          backgroundColor: "blue",
+        }}
+      >
+        <Avatar
+          alt="User profile picture"
+          src={user.person.picture}
+          sx={{ width: 80, height: 80 }}
+        />
       </Box>
       <Box
         sx={{
@@ -38,10 +65,107 @@ const MainCard = () => {
           backgroundColor: "lightblue",
         }}
       >
-        <Button variant="contained" color="primary">
-          Centrar botón
-        </Button>
+        <Typography variant="body1">
+          {user.person.professionalHeadline}
+        </Typography>
       </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "blue",
+        }}
+      >
+        <Typography variant="body1">Skills and interests:</Typography>
+      </Box>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Master</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Chip
+            label="Clickable Link"
+            component="a"
+            href="#basic-chip"
+            variant="outlined"
+            clickable
+          />
+          {user.strengths.map((strength) => (
+            <Chip
+              label={strength.name}
+              component="a"
+              href="#basic-chip"
+              variant="outlined"
+              clickable
+            />
+          ))}
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Expert</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Proficient</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel4a-content"
+          id="panel4a-header"
+        >
+          <Typography>Beginner</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel5a-content"
+          id="panel5a-header"
+        >
+          <Typography>No experience, but interested</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </Container>
   );
 };
